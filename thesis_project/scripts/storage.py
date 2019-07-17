0e2c800341284c8a9422bae1d488b8a64364c1aa
sjnifric <sjnifric@bitbucket.es.uni-due.de>
2019-07-17 13:38:35 +0200

    Introduce timer and control of mcu in storage controller

--- thesis_project/scripts/storage.py
+++ thesis_project/scripts/storage.py
@@ -1,43 +1,69 @@
 """TODO."""
 
-from serial_adapter import SerialAdapter
-from exception_handler import ExceptionHandler
-from filestorage_adapter import FileStorageAdapter
+from timer                  import Timer
+from serial_adapter         import SerialAdapter
+from exception_handler      import ExceptionHandler
+from filestorage_adapter    import FileStorageAdapter
+
+
+FILEPATH            = 'readings.txt'
+PORT_MON            = '/dev/ttyS26'
+PORT_MCU            = '/dev/ttyS22'
+BAUDRATE            = 115200
+START_MON_CMD       = 'C'
+PAUSE_MON_CMD       = 'p'
+STOP_MON_CMD        = 'c'
+SUSPEND_MCU_CMD     = 'S'
+UNSUSPEND_MCU_CMD   = 's'
+RESET_MCU_CMD       = 'R'
+SHUT_ON_MCU_CMD     = 'C'
+SHUT_OFF_MCU_CMD    = 'c'
+LEDFLASH_MCU_CMD    = 'TL'
 
 
 class StorageController:
     """TODO."""
 
-    serialAdapter = None
-    fileStorageAdapter = None
+    fileStorageAdapter      = None
+    serialMonAdapter        = None
+    serialMcuAdapter        = None
+    timer                   = None
 
 
-    def __init__(self, port, baudrate):
+    def __init__(self):
         """TODO."""
-        self.serialAdapter = SerialAdapter(port, baudrate)
+        self.serialMonAdapter   = SerialAdapter(PORT_MON, BAUDRATE)
+        self.serialMcuAdapter   = SerialAdapter(PORT_MCU, BAUDRATE)
         self.fileStorageAdapter = FileStorageAdapter()
+        self.timer              = Timer()
+        self.serialMonAdapter.openSerial()
+        self.serialMcuAdapter.openSerial()
+        self.fileStorageAdapter.openFile(FILEPATH)
     
 
-    def storeSerialDataInFile(self):
+    def storeSerialDataInFile(self, time_limit):
         """TODO."""
-        self.serialAdapter.openSerial()
-        self.fileStorageAdapter.openFile(path='readings.txt')
         try:
-            self.serialAdapter.writeToSerial('C')
-            while True:
+            self.serialMonAdapter.writeToSerial(START_MON_CMD)
+            self.timer.reset_timer()
+            self.serialMcuAdapter.writeToSerial(LEDFLASH_MCU_CMD)
+            while self.timer.get_elapsed_time() < time_limit:
+                # print("Elapsed: %f") % self.timer.get_elapsed_time()
+                # print("Time to run: %f") % (time_limit - self.timer.get_elapsed_time())
                 self.fileStorageAdapter.writeToOpenFile(
-                                                        self.serialAdapter.readFromSerialWithDelay(0.0)
-                                                        )
-                # self.serialAdapter.serialSleep(0.5)
+                                                       self.serialMonAdapter.readFromSerialWithDelay(0.0)
+                                                       )
+                #print( self.serialMonAdapter.readFromSerialWithDelay(0.0))
         except KeyboardInterrupt:
-            self.serialAdapter.writeToSerial('p')
             print("Keyboard interrupt in controller")
         finally:
-            self.serialAdapter.closeSerial()
+            self.serialMonAdapter.writeToSerial(PAUSE_MON_CMD)
             self.fileStorageAdapter.closeFile()
+            self.serialMonAdapter.closeSerial()
+
 
 if __name__ == "__main__":
-    s = StorageController(port='/dev/ttyS26', baudrate=9600)
-    s.storeSerialDataInFile()
+    s = StorageController()
+    s.storeSerialDataInFile(1.0)
 
 
